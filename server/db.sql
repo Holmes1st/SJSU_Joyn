d8b9d0a46b34b60612db1d13abc0444a58458c96
Bang <forresti3979@gmail.com>
2018-07-19 18:20:44 -0700

    add docker-compose & sql

---
+++ server/db.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE IF NOT EXISTS SJSU_TEAM_COMMA_2018;
+
+USE SJSU_TEAM_COMMA_2018;
+
+CREATE TABLE IF NOT EXISTS user (
+    idx     INT(6)  PRIMARY KEY,
+    fname   VARCHAR(30) NOT NULL,
+    lname   VARCHAR(30),
+    email   VARCHAR(50) NOT NULL,
+    pw      CHAR(41)    NOT NULL,
+    birth   DATE,
+    registerdate    DATE
+);
+
+-- CREATE TABLE IF NOT EXISTS SPORTS (
+--     NUM     INT(6) PRIMARY KEY
+--     NAME
+-- );
+--
+-- CREATE TABLE IF NOT EXISTS WHATSPORTUSERWANT (
+--     UNUM
+--     SNUM
+-- );
+--
+-- CREATE TABLE IF NOT EXISTS MATCH (
+--     NUM     INT(6) PRIMARY KEY
+--     UNUM_1
+--     UNUM_2
+--     UNUM_N
+--     SNUM
+--     TIME
+--     RESULT
+-- );
+--
+-- CREATE TABLE IF NOT EXISTS RECORD (
+--
+-- );
